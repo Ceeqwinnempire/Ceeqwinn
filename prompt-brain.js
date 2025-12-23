2279b9c0b0184ed370731fff089d112e7a89fbe4
Ceeqwinnempire <Ceeqwinn123@gmail.com>
2025-12-23 11:23:47 +0100

    Update prompt-brain.js

--- prompt-brain.js
+++ prompt-brain.js
@@ -4,7 +4,7 @@
 
 let sentenceStack = [];
 
-const NEGATIVE_PROMPT = " ⚠️ Negative Prompt: deformed body, extra limbs, extra fingers, distorted face, missing limbs, fused hands, double head, blurry skin, long neck, broken joints, warped anatomy, watermark, text, logo, grain, frame, distortion, cartoonish face, 3D plastic skin, dull lighting, messy background, low quality, cropped, bad anatomy, duplicate limbs, blurred details, out of frame.ge Generation)  negative prompt: deformed body, extra limbs, extra fingers, distorted face, missing limbs, fused hands, double head, blurry skin, long neck, broken joints, warped anatomy, watermark, text, logo, grain, frame, distortion, cartoonish face, 3D plastic skin, dull lighting, messy background, low quality, cropped, bad anatomy, duplicate limbs, blurred details, out of frame. bad anatomy, distortion, extra limbs, low quality, watermark, oversharpening
+const NEGATIVE_PROMPT = "distortion, extra limbs, low quality, watermark, oversharpening
   ";
 
 document.addEventListener("DOMContentLoaded", () => {
